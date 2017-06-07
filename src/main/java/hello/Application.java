bb4132b7290179f1c6a48baf60daf84b98669b15
usman-tahir <tahir.usman.ali94@outlook.com>
2017-06-07 09:10:09 -0400

    added an Application class and a basic Controller class

---
+++ src/main/java/hello/Application.java
@@ -0,0 +1,30 @@
+package hello;
+
+import java.util.Arrays;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class Application {
+
+  @Bean
+  public CommandLineRunner commandLineRunner(ApplicationContext context) {
+    return args -> {
+      System.out.println("Beans provided by Spring Boot:");
+
+      String[] beanNames = context.getBeanDefinitionNames();
+      Arrays.sort(beanNames);
+      for (String beanName : beanNames) {
+        System.out.println(beanName);
+      }
+    };
+  }
+
+  public static void main(String[] args) {
+    SpringApplication.run(Application.class, args);
+  }
+}
